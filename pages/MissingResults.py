42d713092026ac1344893be43aa87a21a8c83aa8
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-05-11 08:37:48 -0700

    Update MissingResults.py

--- pages/MissingResults.py
+++ pages/MissingResults.py
@@ -139,8 +139,8 @@ if cphl is not None and emr is not None:
                     dft = dft[['ART-NO', 'RETURN-DATE','EMR-RESULTS', 'VL_Obs_date','art_number','result_numeric','date_collected', 'COMPARE']]
                     
     if df is not None and df2 is not None: 
-        a = dft.shape[0]
-        st.markdown(f'I see over **{a}** results at CPHL that are not yet entered into EMR')
+              a = dft.shape[0]
+              st.markdown(f'I see over **{a}** results at CPHL that are not yet entered into EMR')
                     
 
 
