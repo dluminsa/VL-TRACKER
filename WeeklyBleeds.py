6220eca7cef768acb0c8b3260f38eb66c37bbb37
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-05-19 16:17:07 -0700

    Update WeeklyBleeds.py

--- WeeklyBleeds.py
+++ WeeklyBleeds.py
@@ -611,7 +611,7 @@ if df is not None:
         
         st.download_button(label='Download VL LINELIST', data=file_contents,file_name=f'VL LINELIST {rand}.xlsx', mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
     #SUBMISSION
-cre =  st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]
+cre =  st.secrets[GOOGLE_APPLICATION_CREDENTIALS]
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 
