078c031a1229fd1880da5d2c5fa6fa68c43a8c02
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-05-23 22:21:28 -0700

    Update WeeklyBleeds.py

--- WeeklyBleeds.py
+++ WeeklyBleeds.py
@@ -10,7 +10,6 @@ import time
 from pathlib import Path
 from google.oauth2.service_account import Credentials
 from oauth2client.service_account import ServiceAccountCredentials
-import configparser
 from streamlit_gsheets import GSheetsConnection
 
 st.set_page_config(
