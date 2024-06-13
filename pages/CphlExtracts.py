a59583a6aaaf036b1dde98adfe535e34dca92a56
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-06-13 02:40:04 -0700

    Update CphlExtracts.py

--- pages/CphlExtracts.py
+++ pages/CphlExtracts.py
@@ -141,6 +141,7 @@ if file and district is not None:
                 st.write(df['dCOL'])
                 st.stop()
                 df['dCOL'] = df['dCOL'].str.replace('-', '*')
+                df['dCOL'] = df['dCOL'].str.replace('/', '*')
                 df[['Dyear', 'Dmonth', 'Dday']] = df['dCOL'].str.split('*', expand=True)
                 df[['Dyear', 'Dmonth', 'Dday']]= df[['Dyear', 'Dmonth', 'Dday']].apply(pd.to_numeric, errors='coerce')
                 df = df[((df['Dyear']==2024) | ((df['Dyear']==2023) & (df['Dmonth']>6)))].copy()
