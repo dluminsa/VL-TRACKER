c2a3023742ebcbb3e115b37556d8e4c5c99edab7
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-11-02 21:30:10 +0300

    Update CphlExtracts.py

--- pages/CphlExtracts.py
+++ pages/CphlExtracts.py
@@ -244,7 +244,6 @@ if file is not None:
                     df['SUP']= df['result_numeric'].apply(Viremia)
                     #factys = dfd[dfd['DISTRICT']==district].copy()
                     facilities = df['facility'].unique()
-                    st.write(facilities)
                     dfdups = df.copy()
                     dfa = []
                     for facility in facilities:
