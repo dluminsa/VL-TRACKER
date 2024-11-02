c2efb9495ef2c8ffd839eef9ccc9c741f3eb4a86
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-11-02 21:45:59 +0300

    Update CphlExtracts.py

--- pages/CphlExtracts.py
+++ pages/CphlExtracts.py
@@ -303,7 +303,7 @@ if file is not None:
                     u = dfc['HLVs'].sum()
                     i = dfc['LLVs'].sum()
                     o = int((t*100)/r)
-
+                    st.write(dfc)
                     dfc.loc[len(dfc), 'facility'] = 'TOTAL'
                     dfc.loc[len(dfc)-1, 'Q3CURR'] = r
                     dfc.loc[len(dfc)-1, 'BLEEDS'] = t
