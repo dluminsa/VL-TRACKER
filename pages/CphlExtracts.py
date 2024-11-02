d10cb31c62805ec0e102bc7ac19091d0186e9ca6
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-11-02 23:10:31 +0300

    Update CphlExtracts.py

--- pages/CphlExtracts.py
+++ pages/CphlExtracts.py
@@ -295,7 +295,7 @@ if file is not None:
                         else:
                             return v
                     dfc['BALANCE TO 95%'] = dfc['BALANCE'].apply(achieve)
-                    dfc = dfc[['facility', 'Q3CURR', 'BLEEDS','VL COV','BALANCE TO 95%', 'HLVs', 'LLVs']]
+                    dfq = dfc[['facility', 'Q3CURR', 'BLEEDS','VL COV','BALANCE TO 95%', 'HLVs', 'LLVs']]
                  
 
                     r = dfq['Q3CURR'].sum()
