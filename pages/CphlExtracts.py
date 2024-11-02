80427c1c8cc03c33476f03cd1a38a6f812ffb26c
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-11-03 01:06:44 +0300

    Update CphlExtracts.py

--- pages/CphlExtracts.py
+++ pages/CphlExtracts.py
@@ -525,6 +525,7 @@ if df is not None and district is not None:
                          'RDO_y':'RDO', 'Dyear_y':'Dyear', 'Dmonth_y':'Dmonth', 'Dday_y':'Dday', 'SUP_y':'SUP'}))
             dfa = dfa[['ART', 'art_number','facility', 'date_collected', 'result_numeric', 'RDO', 'Dyear','Dmonth', 'Dday', 'REBLED']].copy()
             dfsupd = pd.concat([dfa, dups])
+st.write(dfsupd)
 if df is not None and district is not None:       
         def download_without_duplicates(df):
             st.write(f"<h6>CSV FILES for {district} WITH DUPLICATES</h6>", unsafe_allow_html=True)
