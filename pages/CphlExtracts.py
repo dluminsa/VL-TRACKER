ef52da571b26db2e2f6637951e0b86dd7cce31f1
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-11-03 02:12:17 +0300

    Update CphlExtracts.py

--- pages/CphlExtracts.py
+++ pages/CphlExtracts.py
@@ -538,8 +538,8 @@ if df is not None and district is not None:
             if df is not None and district is not None:
                 dft = dfsupd.copy()
                 dft = ppp.copy()
+                st.write(dft)
                 uniques = dft['facility'].unique()
-
                 # Create an expander to contain the download buttons
                 with st.expander(f"Download files for {district} Facilities with duplicates)"):
                     for facility in uniques:
