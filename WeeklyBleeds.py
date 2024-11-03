a9c2768437ec9be0f53f87ca07069584fd57ad31
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-11-03 14:30:11 +0300

    Update WeeklyBleeds.py

--- WeeklyBleeds.py
+++ WeeklyBleeds.py
@@ -43,7 +43,7 @@ if 'tx' not in st.session_state:
          st.stop()
 dftx = st.session_state.tx.copy()
 st.write(dftx.columns)
-dftx[['DUEFN','SUPP', 'NOTFN', 'NOTRN', 'DUERN']] = dftx[['DUEFN','SUPP', 'NOTFN', 'NOTRN' 'DUERN']].apply(pd.to_numeric, errors='coerce')
+#dftx[['DUEFN','SUPP', 'NOTFN', 'NOTRN', 'DUERN']] = dftx[['DUEFN','SUPP', 'NOTFN', 'NOTRN' 'DUERN']].apply(pd.to_numeric, errors='coerce')
 dftx['DUETOTAL'] = dftx['DUEFN'] + dftx['DUERN']
 dftx['TOTAL'] = dftx['DUEFN'] + dftx['DUERN'] +dftx['SUPP'] + dftx['NOTFN'] + dftx['NOTRN']
 st.write(dftx)
