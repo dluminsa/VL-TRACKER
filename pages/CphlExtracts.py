8acc014e0502a7befad22a19bd9887527040fcc6
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-11-03 09:43:48 +0300

    Update CphlExtracts.py

--- pages/CphlExtracts.py
+++ pages/CphlExtracts.py
@@ -625,7 +625,6 @@ if df is not None and district is not None:
                         duern = duedue[duedue['REBLED'] =='RN']
                         durn = duern.shape[0]
                         row1.append(durn)
-                        st.write(row1)
                         sheet1.append_row(row1, value_input_option='RAW')          
             # st.success('Your data above has been submitted')
         except Exception as e:
