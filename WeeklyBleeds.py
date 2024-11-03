494b8b630ec0a7dc8823bfcc7383aa8c10c0cc29
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-11-03 13:30:48 +0300

    Update WeeklyBleeds.py

--- WeeklyBleeds.py
+++ WeeklyBleeds.py
@@ -248,11 +248,10 @@ melted = grouped.melt(id_vars=['WEEK'], value_vars=['DUETOTAL', 'NOTRN', 'SUPP']
        #                     var_name='INTERVAL', value_name='Total')
 melted['WEEK'] = melted['WEEK'].astype(int)
 melted['WEEK'] = melted['WEEK'].astype(str)
-st.write(melted)
 #melted2['SURGE'] = melted2['SURGE'].astype(int)
 #melted2['SURGE'] = melted2['SURGE'].astype(str)
 
-fig2 = px.line(melted, x='WEEK', y='Total', color='INTERVAL', markers=True,
+fig2 = px.line(melted, x='WEEK', y='Total', color='CATEGORIES', markers=True,
               title='REBLEEDING TRENDS', labels={'WEEK':'WEEK', 'Total': 'No. of clients', 'INTERVAL': 'CATEGORIES'})
 
 #fig3 = px.line(melted2, x='SURGE', y='Total', color='INTERVAL', markers=True, color_discrete_sequence=['black','red', 'yellow'],
