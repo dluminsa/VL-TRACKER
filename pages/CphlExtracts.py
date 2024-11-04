fa763ab6de65ac65a046bec2d86dd4c956a9c10b
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-11-04 17:14:42 +0300

    Update CphlExtracts.py

--- pages/CphlExtracts.py
+++ pages/CphlExtracts.py
@@ -21,6 +21,7 @@ def reset_year():
     st.session_state.submited =False
     st.session_state.yra = None
     st.session_state.yr = None
+    st.cache_data()
 SEMBABULE = {'Ssembabule HC IV':2321,'Kyabi HC III':536,'Ntuusi HC IV':968, 'Lwemiyaga HC III':1048,
             'Makoole HC II':252,'Mateete HC III':2367, 'Lwebitakuli Gvt HC III':607,'Ntete HC II':87,'Sembabule Kabaale HC II':77}
 
