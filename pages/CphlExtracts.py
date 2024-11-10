91d849f4ab1e8c37491230e853d4a621ecd7441c
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-11-10 15:42:10 +0300

    Update CphlExtracts.py

--- pages/CphlExtracts.py
+++ pages/CphlExtracts.py
@@ -195,9 +195,8 @@ if file is not None:
                 #     st.stop()
                 # else:
                     facilitys = fac['facility'].unique().tolist()
-                    
+                    st.write(facilitys)
                     df = df[df['facility'].isin(facilitys)].copy()
-                    st.write(df)
                     df['ART'] = df['art_number'].replace('[^0-9]','',regex=True)
                     df['dCOL'] = df['date_collected'].astype(str)
                     df['dCOL'] = df['dCOL'].str.replace('/', '*')
