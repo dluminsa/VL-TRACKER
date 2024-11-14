70281eed8104113685a9756d6b9ea51f8d9c1cf1
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-11-14 10:22:06 +0300

    Update CphlExtracts.py

--- pages/CphlExtracts.py
+++ pages/CphlExtracts.py
@@ -543,7 +543,7 @@ if df is not None and district is not None:
 if not st.session_state.submited:
         st.stop()
 st.write('**FFF***')
-st.write(dfp)       
+st.write(dfe)       
 if st.session_state.submited:
                 @st.cache_data
                 def dupliks():
