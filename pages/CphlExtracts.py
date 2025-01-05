8fd6b403aebed043762c902a840d0cc7b9b57881
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-01-05 07:52:11 +0300

    Update CphlExtracts.py

--- pages/CphlExtracts.py
+++ pages/CphlExtracts.py
@@ -361,7 +361,7 @@ if df is not None and district is not None:
                         
                 #CHECKING IF THE ART NUMBERS ARE NOT IN THE GOOGLE SHHET ARLEADY
                 conn = st.connection('gsheets', type=GSheetsConnection)
-                exist = conn.read(worksheet= 'NS', usecols=list(range(5)),ttl=5)
+                exist = conn.read(worksheet= 'SUP', usecols=list(range(5)),ttl=5)
                 existing= exist.dropna(how='all')
                 st.write(existing)
                 st.stop()
