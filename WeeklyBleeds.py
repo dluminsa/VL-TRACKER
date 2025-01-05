e22fac90c1edc7c27ae4816675e0fb0aa6db234d
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-01-05 23:36:48 +0300

    Update WeeklyBleeds.py

--- WeeklyBleeds.py
+++ WeeklyBleeds.py
@@ -150,7 +150,10 @@ active = dfapt[dfapt['Ryear']==2025]
 ac = active.shape[0]
 lost = dfapt[dfapt['Ryear']<2025]
 los = lost.shape[0]
-st.markdown('**QUICK SUMMARY:**')
+html_table = """
+<h6><b><u style="color: yellow;">QUICK SUMMARY</u></b></h6>
+"""
+st.markdown(html_table, unsafe_allow_html=True)
 
 cola, colb,colc, cold, cole = st.columns(5)
 cola.write('**TOTAL**')
