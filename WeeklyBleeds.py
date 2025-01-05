7f64e4d06d428bdc331533278d9f6caef21e152a
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-01-05 23:14:11 +0300

    Update WeeklyBleeds.py

--- WeeklyBleeds.py
+++ WeeklyBleeds.py
@@ -258,6 +258,7 @@ with st.expander('**CLICK HERE TO DOWNLOAD NS LINELIST**'):
     due = due[['facility','ART','result_numeric', 'date_collected','RD', 'VD', 'TO','DD']].copy()
     due = due.rename(columns = {'RD': 'RETURN DATE', 'VD': 'VL DATE(EMR)', 'TO': 'T/O DATE', 'DD': 'DEATH DATE'})
     due = due.reset_index()
+    due = due.drop(columns = 'index')
     st.write(due.head(5))
     csv_data = due.to_csv(index=False)
     st.download_button(
