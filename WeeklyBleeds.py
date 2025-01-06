e763cf1a988e630d2f3f916f4859f3fae1bc120c
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-01-06 08:01:52 +0300

    Update WeeklyBleeds.py

--- WeeklyBleeds.py
+++ WeeklyBleeds.py
@@ -332,6 +332,7 @@ st.markdown(html_table, unsafe_allow_html=True)
 
 ager = ager[['facility', 'ART', 'AG']].copy()
 ager['AG'] = pd.to_numeric(ager['AG'], errors='coerce')
+st.write(ager)
 def band(x):
    if x < 10:
        return '0 to 9'
