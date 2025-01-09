3494143f1b4e3ee74cce6c6c1f6e88bebe07e82b
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-01-09 11:17:42 +0300

    Update WeeklyBleeds.py

--- WeeklyBleeds.py
+++ WeeklyBleeds.py
@@ -203,6 +203,7 @@ cold.write('**LTFU**')
 cole.write('**T/O**')
 colf.write('**DEAD**')
 for fac in checka:
+    dfapt['USE'] = dfapt['USE'].astype(str)
     dfapta= dfapt[dfapt['USE']==fac].copy()
     totala = dfapta.shape[0]
     deada = dfapta[dfapta['DD'].notna()]
