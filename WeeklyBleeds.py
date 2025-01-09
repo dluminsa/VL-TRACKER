ec2aefc521b33134c7ce132aa10a1ebe2eeee9cb
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-01-09 18:11:55 +0300

    Update WeeklyBleeds.py

--- WeeklyBleeds.py
+++ WeeklyBleeds.py
@@ -178,8 +178,8 @@ ager = active.copy()
 ac = active.shape[0]
 lost = dfapt[dfapt['Ryear']<2025]
 los = lost.shape[0]
-cola, colb = st.columns([1,2])
-colb.columns('**PART ONE: NUMBER OF NS THAT ARE STILL ACTIVE AND DUE**')
+cola, colb = st.columns([1,4])
+colb.write('**PART ONE: NUMBER OF NS THAT ARE STILL ACTIVE AND DUE**')
 
 if len(checkd)>1  or len(checkf) >1:
     html_table = """
