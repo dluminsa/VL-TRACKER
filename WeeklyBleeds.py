1a4751849e8666d583b28a80a79bf6360d077df6
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-01-09 12:15:59 +0300

    Update WeeklyBleeds.py

--- WeeklyBleeds.py
+++ WeeklyBleeds.py
@@ -174,7 +174,7 @@ ac = active.shape[0]
 lost = dfapt[dfapt['Ryear']<2025]
 los = lost.shape[0]
 
-if CLUSTER or district and not facility:
+if len(checkd)>1:
     html_table = """
     <h6><b><u style="color: purple;">QUICK SUMMARY</u></b></h6>
     """
