bcbcf34f170f00c59d683845e9ef6992ed6488ac
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-01-09 10:42:17 +0300

    Update WeeklyBleeds.py

--- WeeklyBleeds.py
+++ WeeklyBleeds.py
@@ -195,6 +195,7 @@ html_table = """
     <h6><b><u style="color: purple;">QUICK SUMMARY BY DISTRICT/FACILITY</u></b></h6>
     """
 st.markdown(html_table, unsafe_allow_html=True)
+cola, colb,colc, cold, cole, colf = st.columns(6)
 for fac in checka:
     dfapt = dfapt[dfapt['USE']==fac].copy()
     st.write(f'**{word}**')
