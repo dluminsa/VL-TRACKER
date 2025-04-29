cf5319d595df021437a1b87b602f79890ff479d7
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-04-29 21:07:30 +0300

    Update CphlExtracts.py

--- pages/CphlExtracts.py
+++ pages/CphlExtracts.py
@@ -413,7 +413,7 @@ if df is not None and district is not None:
                         return 'background-color: red'
                 styler = (
                             dfe.style
-                            .format("{:.0f}", subset = ['VL COV','BLEEDS','Q3CURR', 'BALANCE TO 95%', 'HLVs', 'LLVs'])  # Format 'VL COV' to one decimal place
+                            .format("{:.0f}", subset = ['VL COV','BLEEDS','Q1CURR', 'BALANCE TO 95%', 'HLVs', 'LLVs'])  # Format 'VL COV' to one decimal place
                             .applymap(kusiiga, subset=['VL COV'])  # Apply cell-wise styling
                         )
                 #st.stop()
