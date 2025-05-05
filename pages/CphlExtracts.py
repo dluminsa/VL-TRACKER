1b7ecc055170730e10ff61fb0ec084ad73b24150
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-05-05 13:01:07 +0300

    Update CphlExtracts.py

--- pages/CphlExtracts.py
+++ pages/CphlExtracts.py
@@ -397,7 +397,7 @@ if df is not None and district is not None:
                 # else:
                 #         pass
                         
-
+st.write(dfe)
 if df is not None and district is not None:
 
                 dfe = dfe.drop(columns = 'DISTRICT')
